3a9c7c3a4c14b20a229cb13c759b102b95acd8d9
Erdaulet <47812867+Can1s@users.noreply.github.com>
2020-08-07 16:15:19 +0600

    Stage 2/6
    Completed the second stage

---
+++ coffee_machine.py
@@ -0,0 +1,6 @@
+print("Write how many cups of coffee you will need:")
+cups_of_coffee = int(input())
+print(f"""For {cups_of_coffee} cups of coffee you will need:
+{cups_of_coffee * 200} ml of water
+{cups_of_coffee * 50} ml of milk
+{cups_of_coffee * 15} g of coffee beans""")
